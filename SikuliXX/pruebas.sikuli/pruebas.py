4867412c4034d127b98400be110fe469e492cbd8
nahumsin <nahumaldonado@gmail.com>
2014-12-12 10:13:44 -0600

    Agregando pruebas de interfaz

---
+++ SikuliXX/pruebas.sikuli/pruebas.py
@@ -0,0 +1,22 @@
+click("1418364304646.png")
+type("juanGomez")
+click("1418364433663.png")
+type("cartmancuco")
+click("1418364470829.png")
+
+if exists("1418366374177.png"):
+    click("1418366207707.png")
+    type("Augusto")
+    click("1418366216665.png")
+    type("Casares")
+    click("1418366224563.png")
+    type("30")
+    click("1418366231210.png")
+    type("augustocasaresgmail.com")
+    click("1418366238035.png")
+    type("Paseo de la canada 103")
+    click("1418366855052.png")
+    print("Paciente agregado correctamente")
+
+else:
+    print("No existe el nombre de usuario")
